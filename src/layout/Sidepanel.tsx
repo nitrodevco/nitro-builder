434883b7d9d7a3bc7c1015a4d4046f4497ef11c6
robbis95 <82042925+robbis95@users.noreply.github.com>
2024-02-24 11:08:24 +0100

    Added dotted line when dragging

--- src/layout/Sidepanel.tsx
+++ src/layout/Sidepanel.tsx
@@ -61,15 +61,17 @@ export const Sidepanel: FC<SidepanelProps> = ({ id, title, content, index, moveP
     drag(drop(ref));
 
     return (
-        <div ref={ ref } style={ { opacity: isDragging ? 0 : 1 } }>
-            <Flex column className="flex bg-[#535353] w-full rounded-tr-md rounded-tl-md border border-[#636363] transition-all">
-                <div className="cursor-move w-full bg-[#424242] text-white font-bold text-[10px] rounded-tr-md rounded-tl-md h-[15px] flex justify-center items-center">
-                    { title }
-                </div>
-                <Flex column className="text-[12px] flex-1 h-full p-3 gap-1">
-                    { content }
+        <div ref={ ref } style={ { border: isDragging ? '2px dotted #292929' : '' } }>
+            <div style={ { opacity: isDragging ? 0 : 1 } }>
+                <Flex column className="flex bg-[#535353] w-full rounded-tr-md rounded-tl-md border border-[#636363] transition-all">
+                    <div className="cursor-move w-full bg-[#424242] text-white font-bold text-[10px] rounded-tr-md rounded-tl-md h-[15px] flex justify-center items-center">
+                        { title }
+                    </div>
+                    <Flex column className="text-[12px] flex-1 h-full p-3 gap-1">
+                        { content }
+                    </Flex>
                 </Flex>
-            </Flex>
+            </div>
         </div>
     );
 };
